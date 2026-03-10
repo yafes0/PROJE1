ff5fb12f52b0d9d1477ccfb3d771717806350d59
YAFES <YAFES@DESKTOP-6H4TR0S>
2026-03-11 00:56:17 +0300

    Proje dosyası ekle.

---
+++ PROJE1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PROJE1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int SAYI1;
+            int SAYI2;
+            string METİN1;
+            string METİN2;
+            int SONUÇ;
+
+            Console.WriteLine("1.Sayıyı giriniz:");
+            METİN1 = Console.ReadLine();
+            SAYI1 = Convert.ToInt32(METİN1);
+
+            Console.WriteLine("2. Sayıyı giriniz:");
+            METİN2 = Console.ReadLine();
+            SAYI2 = Convert.ToInt32(METİN2);
+
+            SONUÇ = SAYI1 + SAYI2;
+
+            Console.WriteLine(SONUÇ.ToString());
+        }
+    }
+}
